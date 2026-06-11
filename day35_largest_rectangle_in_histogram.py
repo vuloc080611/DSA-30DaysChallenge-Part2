9158b7c74772e27b575545b384681bf5b8048bed
only4u <vuloc668165@gmail.com>
2026-06-11 18:05:22 +0700

    Implement largest rectangle in histogram solution

--- day35_largest_rectangle_in_histogram.py
+++ day35_largest_rectangle_in_histogram.py
@@ -1,1 +1,19 @@
+from typing import List
 
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        heights.append(0)  # Sentinel
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+        heights.pop()
+        return max_area
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.largestRectangleArea([2,1,5,6,2,3]))  # 10
