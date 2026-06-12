42a3652ededfbc5c8dc2916a8da8e645826c1a57
only4u <vuloc668165@gmail.com>
2026-06-12 21:12:01 +0700

    Implement maximal rectangle calculation in matrix

--- day36_maximal_rectangle.py
+++ day36_maximal_rectangle.py
@@ -1,1 +1,33 @@
+from typing import List
 
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        heights = [0] * n
+        max_area = 0
+
+        for i in range(m):
+            for j in range(n):
+                heights[j] = heights[j] + 1 if matrix[i][j] == '1' else 0
+            max_area = max(max_area, self.largestRectangleArea(heights))
+        return max_area
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        heights.append(0)
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+        heights.pop()
+        return max_area
+
+if __name__ == "__main__":
+    s = Solution()
+    matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+    print(s.maximalRectangle(matrix))  # 6
