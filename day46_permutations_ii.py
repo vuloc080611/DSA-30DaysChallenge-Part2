75d01917a58a99e538e71d6ab235358f1ed8d9b0
only4u <vuloc668165@gmail.com>
2026-06-14 13:56:31 +0700

    Update day46_permutations_ii.py

--- day46_permutations_ii.py
+++ day46_permutations_ii.py
@@ -1,1 +1,25 @@
+from typing import List
 
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i] or (i > 0 and nums[i] == nums[i-1] and not used[i-1]):
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+                used[i] = False
+        backtrack([])
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.permuteUnique([1,1,2]))
