286703c712f84e2089d071fe69a863f70506cd3c
only4u <vuloc668165@gmail.com>
2026-06-14 13:55:20 +0700

    Implement subsetsWithDup method for generating subsets

--- day44_subsets_ii.py
+++ day44_subsets_ii.py
@@ -1,1 +1,20 @@
+from typing import List
 
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        def backtrack(start, path):
+            res.append(path[:])
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                backtrack(i+1, path)
+                path.pop()
+        backtrack(0, [])
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.subsetsWithDup([1,2,2]))
