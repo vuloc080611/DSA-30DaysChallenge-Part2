beea7c24885e4aadc36c4c00c7ab2fcd34e6d68c
only4u <vuloc668165@gmail.com>
2026-06-14 13:54:41 +0700

    Implement word ladder length calculation

--- day42_word_ladder.py
+++ day42_word_ladder.py
@@ -1,1 +1,27 @@
+from typing import List
+from collections import deque
 
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        queue = deque([(beginWord, 1)])
+        while queue:
+            word, steps = queue.popleft()
+            if word == endWord:
+                return steps
+            for i in range(len(word)):
+                original_char = word[i]
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    if c == original_char:
+                        continue
+                    next_word = word[:i] + c + word[i+1:]
+                    if next_word in wordSet:
+                        wordSet.remove(next_word)
+                        queue.append((next_word, steps + 1))
+        return 0
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))  # 5
