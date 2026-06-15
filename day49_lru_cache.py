e3779606e8db1b7adc3f4dfe9b4e62cb0f08c4ed
only4u <vuloc668165@gmail.com>
2026-06-15 19:04:32 +0700

    Update day49_lru_cache.py

--- day49_lru_cache.py
+++ day49_lru_cache.py
@@ -1,1 +1,6 @@
-
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache là {1=1}
+lRUCache.put(2, 2); // cache là {1=1, 2=2}
+lRUCache.get(1);    // trả về 1
+lRUCache.put(3, 3); // vượt dung lượng, xóa key 2, cache là {1=1, 3=3}
+lRUCache.get(2);    // trả về -1 (không tìm thấy)
