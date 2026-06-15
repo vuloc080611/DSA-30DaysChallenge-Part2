991fc980405590c1e368548786d7bf52a5da8eea
only4u <vuloc668165@gmail.com>
2026-06-15 19:09:24 +0700

    Add singleNumber method to find unique element
    Implement single number algorithm using bit manipulation.

--- day55_single_number_ii.py
+++ day55_single_number_ii.py
@@ -1,1 +1,14 @@
+from typing import List
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Dùng bit mask: ones, twos
+        ones = twos = 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.singleNumber([2,2,3,2]))  # 3
