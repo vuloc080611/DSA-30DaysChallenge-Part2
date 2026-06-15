f99ec3dd2f57ed8ea5167d042bb56da934acab9e
only4u <vuloc668165@gmail.com>
2026-06-15 19:09:09 +0700

    Update day54_sqrt_x.py

--- day54_sqrt_x.py
+++ day54_sqrt_x.py
@@ -1,1 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 2, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
 
+if __name__ == "__main__":
+    s = Solution()
+    print(s.mySqrt(8))  # 2
