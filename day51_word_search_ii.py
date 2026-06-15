da2b970b22de56a4ad8206bc097ebf972acd7079
only4u <vuloc668165@gmail.com>
2026-06-15 19:08:27 +0700

    Add Trie-based word search solution
    Implemented a Trie data structure to efficiently search for words in a 2D board using DFS.

--- day51_word_search_ii.py
+++ day51_word_search_ii.py
@@ -1,1 +1,47 @@
+from typing import List
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Xây dựng Trie
+        root = TrieNode()
+        for w in words:
+            node = root
+            for ch in w:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            node.word = w
+
+        m, n = len(board), len(board[0])
+        res = []
+
+        def dfs(r, c, node):
+            ch = board[r][c]
+            if ch not in node.children:
+                return
+            next_node = node.children[ch]
+            if next_node.word:
+                res.append(next_node.word)
+                next_node.word = None  # đánh dấu đã lấy
+            board[r][c] = '#'  # đánh dấu đã thăm
+            for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] != '#':
+                    dfs(nr, nc, next_node)
+            board[r][c] = ch
+
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+    words = ["oath","pea","eat","rain"]
+    print(s.findWords(board, words))  # ["eat","oath"] (thứ tự có thể khác)
