aae2f7334af4fd5fc0b6b3fd9dde2f75cf859dd7
only4u <vuloc668165@gmail.com>
2026-06-16 10:42:06 +0700

    Add numSquares method using dynamic programming
    Implement dynamic programming solution to find the minimum number of perfect squares that sum to n.

--- day60_perfect_squares.py
+++ day60_perfect_squares.py
@@ -1,1 +1,17 @@
+import math
 
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # DP cổ điển
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - j*j] + 1)
+                j += 1
+        return dp[n]
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.numSquares(12))  # 3
