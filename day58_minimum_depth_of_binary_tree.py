ccb34eabdb688a0dd553395846c3da37b355b453
only4u <vuloc668165@gmail.com>
2026-06-16 10:40:58 +0700

    Implement minimum depth calculation for binary tree

--- day58_minimum_depth_of_binary_tree.py
+++ day58_minimum_depth_of_binary_tree.py
@@ -1,1 +1,26 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        q = deque([(root, 1)])
+        while q:
+            node, depth = q.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                q.append((node.left, depth+1))
+            if node.right:
+                q.append((node.right, depth+1))
+        return 0
+
+if __name__ == "__main__":
+    s = Solution()
+    root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+    print(s.minDepth(root))  # 2
